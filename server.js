f2a4776e4763a657c5d9dff600a6aad6aedfd9fb
Irina Saveleva <goyalion17@gmail.com>
2023-03-02 20:20:21 +0100

    routing

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
+let requestCounter = 0;
+
 const server = http.createServer((request, response) => {
-    response.write('hello world')
+    requestCounter++;
+
+    switch (request.url) {
+        case '/students':
+            response.write('STUDENTS')
+            break;
+        case '/courses':
+            response.write('FRONT + BACK')
+            break;
+        default:
+            response.write('404 not found')
+            }
+
+    response.write(' Hello world: ' + requestCounter)
     response.end()
 })
 
